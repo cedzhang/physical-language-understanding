1b4ada3d758910f52a0654406472586b8860a2d2
cedzhang <cedzhang@mit.edu>
2023-01-23 17:33:55 -0500

    Improve generative model

--- webppl/generative_model.js
+++ webppl/generative_model.js
@@ -2,8 +2,12 @@
 // Distributions
 //////////////////////////////
 
-var geometric = function(p) {
-  return flip(p) ? 1 + geometric(p) : 1
+var truncGeom = function(p, m, n) {
+  if (m > n) {
+    return uniformDraw(_.range(1, n+1));
+  } else {
+    return flip(p) ? truncGeom(p, m+1, n) : m;
+  }
 }
 
 //////////////////////////////
@@ -11,14 +15,17 @@ var geometric = function(p) {
 //////////////////////////////
 
 // var dim = function() { return uniform(5, 10) }
-var dim = 7.5;
-var color = function() { return flip() ? 'red' : 'yellow' }
-var stackHeight = function() { return geometric(0.7) }
-// var numStacks = function() {return sample(Poisson({mu: 3.5}))}
-var numStacks = function() {return 2}
+var dim = 10;
+var tableSize = 100;
+var color = function() { return flip() ? 'red' : 'yellow' };
+var stackHeight = function() { return truncGeom(0.9, 1, 8) };
+var numStacks = function() {return sample(Poisson({mu: 3}))+1};
 
-var xposOnTable = function() { 
-  return uniform(worldWidth/2 - 100, worldWidth/2 + 100) }
+var xpositions = _.range(worldWidth/2 - tableSize, worldWidth/2 + tableSize + 20, 20);
+
+var xposOnTable = function(options) { 
+  return uniformDraw(options); 
+}
 
 var ground = {shape: 'rect',
   static: true,
@@ -29,7 +36,7 @@ var ground = {shape: 'rect',
 
 var table = {shape: 'rect',
   static: false,
-  dims: [100, 100],
+  dims: [tableSize, tableSize],
   x: worldWidth/2,
   y: 390,
   color: 'blue'
@@ -37,55 +44,73 @@ var table = {shape: 'rect',
 
 // Forces
 
-var leftCircle = {shape: 'circle',
-  static: false,
-  dims: [20],
-  x: 100,
-  y: 310,
-  color: 'green',
-  velocity: [10000, 0] 
+var force = function() {
+  var left = flip();
+  return {
+    shape: 'square',
+    static: false,
+    dims: [uniformDraw([15, 20])],
+    x: left? 100 : worldWidth - 100,
+    y: 310,
+    color: 'green',
+    velocity: left? [10000, 0] : [-10000, 0]
+  }
 }
 
-var rightCircle = {shape: 'circle',
-  static: false,
-  dims: [20],
-  x: worldWidth - 100,
-  y: 310,
-  color: 'green',
-  velocity: [-10000, 0] 
-}
+// var leftCircle = {shape: 'square',
+//   static: false,
+//   dims: [15],
+//   x: 100,
+//   y: 310,
+//   color: 'green',
+//   velocity: [10000, 0] 
+// }
+
+// var rightCircle = {shape: 'square',
+//   static: false,
+//   dims: [15],
+//   x: worldWidth - 100,
+//   y: 310,
+//   color: 'green',
+//   velocity: [-10000, 0] 
+// }
 
 //////////////////////////////
 // Make stacks
 //////////////////////////////
 
-var xpos = function (prevBlock) {
+var xpos = function(prevBlock) {
   return prevBlock.x
 }
 
-var ypos = function (prevBlock, h) {
+var ypos = function(prevBlock, h) {
   var prevY = prevBlock.y;
   var prevH = prevBlock.dims[1];
   return prevY - (prevH + h)
 }
 
 var addBlock = function(prevBlock, isFirst) {
+  var xposition = xposOnTable(xpositions);
+  if (isFirst) {
+    console.log("First");
+    var index = xpositions.indexOf(xposition);
+    xpositions.splice(index, 1);
+    console.log(xpositions);
+  }
   return {
     shape: 'rect',
     color: isFirst? color() : prevBlock.color,
     static: false,
     dims: [dim, dim],
-    x: isFirst? xposOnTable() : xpos(prevBlock),
+    x: isFirst? xposition : xpos(prevBlock),
     y: ypos(prevBlock, dim)
   }
 }
-
 
 var stack = function(stackSoFar, h) {
   var newBlock = addBlock(stackSoFar[stackSoFar.length - 1], false);
   var theStack = stackSoFar.concat(newBlock);
   if (stackSoFar.length >= h) {
-    console.log()
     return stackSoFar;
   } else {
     return stack(theStack, h);
@@ -108,12 +133,15 @@ var makeStacks = function(stacksSoFar, n) {
 // Make world
 //////////////////////////////
 
+
+// make world
+
 var makeBlockWorld = function() {
   var n = numStacks();
+  console.log(n);
   var stacks = makeStacks([], n);
-  var force = flip()? leftCircle : rightCircle;
   var blockList = stacks.flat();
-  var world = [ground, table, force].concat(blockList);
+  var world = [ground, table, force()].concat(blockList);
 //   var world = [ground, table, force]
   return world;
 }
@@ -130,8 +158,6 @@ var tall = function(obj) {
   return obj.y < 250;
 }
 
-
-
 var run = function(world) {
   condition(filter(red, world).length >= 1);
 //   condition(any(tall, world));
@@ -144,5 +170,4 @@ var run = function(world) {
 //            function() { return run(makeBlockWorld()) }))
 
 //Simulating and animating the world
-
 physics.animate(1000, makeBlockWorld());
