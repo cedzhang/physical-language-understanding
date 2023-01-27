eb6cc6927b2f42d4fe7cf4ffe423fc536bc47781
Gabe Grand <grandg@mit.edu>
2023-01-27 17:38:43 -0500

    cleanup

--- webppl/generative_model.js
+++ webppl/generative_model.js
@@ -1,92 +1,91 @@
-var makeBlockWorld = function() {
+var makeBlockWorld = function () {
 
   //// Distributions and parameters ////
 
-  var truncGeom = function(p, m, n) {
+  var truncGeom = function (p, m, n) {
     if (m > n) {
-      return uniformDraw(_.range(1, n+1));
+      return uniformDraw(_.range(1, n + 1));
     } else {
-      return flip(p) ? truncGeom(p, m+1, n) : m;
+      return flip(p) ? truncGeom(p, m + 1, n) : m;
     }
   }
 
-//   var dim = function() { return uniform(5, 10) }
   var dim = 10;
   var tableSize = 100;
   var worldWidth = 600;
   var worldHeight = 500;
-  var color = function() { return flip() ? 'red' : 'yellow' };
+  var color = function () { return flip() ? 'red' : 'yellow' };
   var monoColor = flip();
-  var stackHeight = function() { return truncGeom(0.5, 1, 8) };
-//   var numStacks = function() {return sample(Poisson({mu: 3}))+1};
-//   var numStacks = uniformDraw(_.range(1, 9));
+  var stackHeight = function () { return truncGeom(0.5, 1, 8) };
   var numStacks = truncGeom(0.5, 1, 8);
-  var xpositions = _.range(worldWidth/2 - tableSize, worldWidth/2 + tableSize + 20, 20);
+  var xpositions = _.range(worldWidth / 2 - tableSize, worldWidth / 2 + tableSize + 20, 20);
 
   //// Object definitions ////
 
-  var ground = {shape: 'rect',
-                static: true,
-                dims: [100000*worldWidth, 10],
-                x: worldWidth/2,
-                y: worldHeight
-               }
-
-  var table = {shape: 'rect',
-               static: false,
-               dims: [tableSize, tableSize],
-               x: worldWidth/2,
-               y: 390,
-               color: 'blue'
-              }
-
-  var force = function() {
-    var left = flip();
-    return {shape: 'square',
-            static: false,
-            dims: [uniformDraw([15, 16, 17, 18])],
-            x: left? 100 : worldWidth - 100,
-            y: 310,
-            color: 'green',
-            velocity: left? [10000, 0] : [-10000, 0]
-           }
+  var ground = {
+    shape: 'rect',
+    static: true,
+    dims: [100000 * worldWidth, 10],
+    x: worldWidth / 2,
+    y: worldHeight
   }
 
-//// Make stacks ////
+  var table = {
+    shape: 'rect',
+    static: false,
+    dims: [tableSize, tableSize],
+    x: worldWidth / 2,
+    y: 390,
+    color: 'blue'
+  }
 
-  var xposOnTable = function(options) {
+  var force = function () {
+    var left = flip();
+    return {
+      shape: 'square',
+      static: false,
+      dims: [uniformDraw([15, 16, 17, 18])],
+      x: left ? 100 : worldWidth - 100,
+      y: 310,
+      color: 'green',
+      velocity: left ? [10000, 0] : [-10000, 0]
+    }
+  }
+
+  //// Make stacks ////
+
+  var xposOnTable = function (options) {
     return uniformDraw(options);
   }
 
-  var xpos = function(prevBlock) {
+  var xpos = function (prevBlock) {
     return prevBlock.x
   }
 
-  var ypos = function(prevBlock, h) {
+  var ypos = function (prevBlock, h) {
     var prevY = prevBlock.y;
     var prevH = prevBlock.dims[1];
     return prevY - (prevH + h)
   }
 
-  var addBlock = function(prevBlock, isFirst) {
+  var addBlock = function (prevBlock, isFirst) {
     var xposition = xposOnTable(xpositions);
     if (isFirst) {
-//       console.log("First");
       var index = xpositions.indexOf(xposition);
       xpositions.splice(index, 1);
-//       console.log(xpositions);
     }
-    return {shape: 'rect',
-            color: isFirst || !monoColor? color() : prevBlock.color,
-            static: false,
-            dims: [dim, dim],
-            x: isFirst? xposition : xpos(prevBlock),
-            y: ypos(prevBlock, dim)
-           }
+    return {
+      shape: 'rect',
+      color: isFirst || !monoColor ? color() : prevBlock.color,
+      static: false,
+      dims: [dim, dim],
+      x: isFirst ? xposition : xpos(prevBlock),
+      y: ypos(prevBlock, dim)
+    }
   }
 
-  var stack = function(stackSoFar, h) {
-    var newBlock = addBlock(stackSoFar[stackSoFar.length-1], false);
+  var stack = function (stackSoFar, h) {
+    var newBlock = addBlock(stackSoFar[stackSoFar.length - 1], false);
     var theStack = stackSoFar.concat(newBlock);
     if (stackSoFar.length >= h) {
       return stackSoFar;
@@ -95,12 +94,10 @@ var makeBlockWorld = function() {
     }
   }
 
-  var makeStacks = function(stacksSoFar, n) {
+  var makeStacks = function (stacksSoFar, n) {
     var height = stackHeight();
     var newStack = stack([addBlock(table, true)], height);
     var stacks = stacksSoFar.concat([newStack]);
-//     var index = xpositions.indexOf(stacks[stacks.length - 1][0].x);
-//     xpositions.splice(index, 1);
     if (stacksSoFar.length >= n) {
       return stacksSoFar;
     } else {
@@ -108,17 +105,18 @@ var makeBlockWorld = function() {
     }
   }
 
-  var getStackInfo = function(stack) {
-    var color = monoColor? stack[0].color : 'mixed';
-    var height = stack[0].y - stack[stack.length-1].y + 2 * dim;
-    return {blocks: stack,
-            color: color,
-            x: stack[0].x,
-            height: height
-           }
+  var getStackInfo = function (stack) {
+    var color = monoColor ? stack[0].color : 'mixed';
+    var height = stack[0].y - stack[stack.length - 1].y + 2 * dim;
+    return {
+      blocks: stack,
+      color: color,
+      x: stack[0].x,
+      height: height
+    }
   }
 
-  var realStack = function(stack) {
+  var realStack = function (stack) {
     return stack.length >= 2;
   }
 
@@ -128,46 +126,111 @@ var makeBlockWorld = function() {
   var blockList = rawStacks.flat();
   var rawStacks = filter(realStack, rawStacks);
   var stacks = map(getStackInfo, rawStacks);
-  var world = {stacks: stacks,
-               blocks: blockList,
-               ground: ground,
-               table: table,
-               force: force()
-              }
-  var world = [ground, table, force()].concat(blockList);
-//   var world = [ground, table, force]
+  var world = {
+    stacks: stacks,
+    blocks: blockList,
+    ground: ground,
+    table: table,
+    force: force()
+  }
   return world;
 }
 
-// physics.animate(1000, makeBlockWorld());
+///////////////////
+//// Semantics ////
+///////////////////
 
-var run = function(world) {
-//   condition(filter(red, world).length >= 1);
-//   condition(any(tall, world));
-  var finalWorld = physics.run(1000, world);
-//   return finalWorld[0].velocity[0] > 0;
-//   return filter(red, finalWorld).length;
-  var objectsOnGround = filter(onGround, finalWorld);
-  var numYellow = filter(isYellow, objectsOnGround).length
-  var numRed = filter(isRed, objectsOnGround).length
-  return numYellow > numRed? 2 : numYellow == numRed? 1 : 0
-}
-
-var isRed = function(obj) {
+var isRed = function (obj) {
   return obj.color == 'red';
 }
 
-var isYellow = function(obj) {
+var isYellow = function (obj) {
   return obj.color == 'yellow';
 }
 
-var isTall = function(obj) {
-  return obj.y < 160;
+var isTall = function (stack) {
+  return stack.height >= 100;
 }
 
-var onGround = function(obj) {
+var isShort = function (stack) {
+  return stack.height <= 60;
+}
+
+var isOnLeft = function (obj) {
+  return obj.x <= 270;
+}
+
+var isOnRight = function (obj) {
+  return obj.x >= 330;
+}
+
+var isOnMiddle = function (obj) {
+  return !isOnLeft(obj) && !isOnRight(obj);
+}
+
+var isOnEdge = function (obj) {
+  return obj.x == 200 || obj.x == 400;
+}
+
+var isOnCenter = function (obj) {
+  return obj.x == 300;
+}
+
+var isOnGround = function (obj) {
   return obj.y > 400;
 }
 
+var isNear = function (obj1) {
+  return function (obj2) {
+    return Math.abs(obj1.x - obj2.x) <= 40;
+  }
+}
 
-Infer({method: 'forward', samples: 100}, function() { return run(makeBlockWorld()) })
+/////////////////////////////////////
+//// Visualization and inference ////
+/////////////////////////////////////
+
+var flattenWorld = function (world) {
+  return [world.ground, world.table, world.force].concat(world.blocks);
+}
+
+var visualizeConds = function (world) {
+  var flatWorld = flattenWorld(world);
+  var finalWorld = physics.run(1, flatWorld);
+  return finalWorld;
+}
+
+var w = Infer({ method: 'rejection', samples: 1 },
+  function () { return visualizeConds(makeBlockWorld()) });
+var theWorld = w.toString().slice(14, -4);
+
+var run = function (world) {
+  var flatWorld = flattenWorld(world);
+  var finalWorld = physics.run(1000, flatWorld);
+
+  var objectsOnGround = filter(isOnGround, finalWorld);
+  condition(objectsOnGround.length > 2);
+
+  var numYellow = filter(isYellow, objectsOnGround).length;
+  var numRed = filter(isRed, objectsOnGround).length;
+  return numYellow > numRed ? 2 : numYellow == numRed ? 1 : 0
+}
+
+var result = function () {
+  var d = Infer({ method: 'rejection', samples: 10 },
+    function () { return run(makeBlockWorld()) });
+  var moreRedProb = Math.exp(d.score(0));
+  var moreYellowProb = Math.exp(d.score(2));
+  var likert = Math.round((moreYellowProb / (moreRedProb + moreYellowProb)) * 6) + 1;
+  return likert;
+}
+
+var dist = Infer({ method: 'forward', samples: 10 }, result);
+var getData = function (key) {
+  return Math.exp(dist.score(key));
+}
+var output = {
+  "probs": map(getData, dist.support()),
+  "support": dist.support()
+}
+output
